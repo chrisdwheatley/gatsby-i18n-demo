256468826dea7cae0d3b63dabeb0d5598e98789d
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2020-01-23 09:34:55 +0000

    fix ssr

--- src/components/locale-checker.js
+++ src/components/locale-checker.js
@@ -2,11 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 const LocaleChecker = ({ locale, locales, path }) => {
-  if (navigator) {
+  if (typeof window !== 'undefined') {
     if (navigator.language !== locale.path) {
       const currentLocale = locales.filter(loc => loc.path === navigator.language)[0]
+
       if (currentLocale) {
-        return <div>
+        return <div style={{ color: 'white' }}>
           Switch to the <Link style={{ color: 'white' }} to={currentLocale.default ? '/' : `/${currentLocale.path}`}>{currentLocale.flag} {currentLocale.title}</Link> version?
         </div>
       }
