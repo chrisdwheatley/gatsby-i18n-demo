b8949df739fdab94e4cc6edb367ced19b52ac8d9
Chris Draycott-Wheatley <chriswheatley87@gmail.com>
2020-01-24 13:40:09 +0000

    add styles to homepage

--- src/templates/home.js
+++ src/templates/home.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import "../components/layout.css"
 
 const Article = (props) => {
   const data = useStaticQuery(graphql`
